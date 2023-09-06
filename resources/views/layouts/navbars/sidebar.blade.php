5ebe99c1a8c9a73a2ffcee7e3d0598c24d0ab2b3
Pato Laptop <spfautschb@gmail.com>
2023-09-06 09:19:15 -0600

    fixed menus

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -5,7 +5,7 @@
       Tip 2: you can also add an image using data-image tag
   -->
   <div class="logo">
-    <a href="/" class="simple-text logo-normal">
+    <a href="/home" class="simple-text logo-normal">
       {{ __('Cotizador') }}
     </a>
   </div>
