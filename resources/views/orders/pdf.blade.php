633211e25fc7220359b00490affa6ca2adf0e74c
PatoFb <spfautschb@gmail.com>
2023-10-06 12:51:50 -0600

    initial pdf config

--- resources/views/orders/pdf.blade.php
+++ resources/views/orders/pdf.blade.php
@@ -1,13 +1,61 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Order Details</title>
+    <title>Detalles de orden</title>
 </head>
 <body>
-<h1>Order Details</h1>
-<p>Order ID: {{ $order->id }}</p>
+<h1>Orden {{ $order->id }}</h1>
 <p>Customer Name: {{ $order->user->name }}</p>
-<!-- Add more order details here -->
+<div>
+    <table>
+        <thead>
+            <tr>
+                <th>Datos:</th>
+                @for($i = 1; $i <= sizeof($order->curtains); $i++)
+                    <th>Sistema {{$i}}:</th>
+                @endfor
+            </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Cantidad</td>
+            @foreach($order->curtains as $curtain)
+                <td>{{$curtain->quantity}}</td>
+            @endforeach
+        </tr>
+        <tr>
+            <td>Modelo</td>
+            @foreach($order->curtains as $curtain)
+                <td>{{$curtain->model->name}}</td>
+            @endforeach
+        </tr>
+        <tr>
+            <td>Cubierta</td>
+            @foreach($order->curtains as $curtain)
+                <td>{{$curtain->cover->name}}</td>
+            @endforeach
+        </tr>
+        <tr>
+            <td>Mecanismo</td>
+            @foreach($order->curtains as $curtain)
+                <td>{{$curtain->mechanism->name}}</td>
+            @endforeach
+        </tr>
+        <tr>
+            <td>Ancho</td>
+            @foreach($order->curtains as $curtain)
+                <td>{{$curtain->width}} m</td>
+            @endforeach
+        </tr>
+        <tr>
+            <td>Caída</td>
+            @foreach($order->curtains as $curtain)
+                <td>{{$curtain->height}} m</td>
+            @endforeach
+        </tr>
+        </tbody>
+    </table>
+</div>
 </body>
 </html>
 
