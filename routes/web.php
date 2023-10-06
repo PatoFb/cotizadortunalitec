70969bf86915c574b4cd812df995c2fb8cde18b1
PatoFb <spfautschb@gmail.com>
2023-10-06 13:57:44 -0600

    upload route

--- routes/web.php
+++ routes/web.php
@@ -58,8 +58,6 @@ Route::group(['middleware' => 'admin'], function () {
 
     Route::get('orders/{id}/close', 'App\Http\Controllers\OrdersController@close')->name('orders.close');
 
-    Route::post('orders/{id}/upload', 'App\Http\Controllers\OrdersController@upload')->name('orders.upload');
-
     Route::get('orders/{id}/download', 'App\Http\Controllers\OrdersController@download')->name('orders.download');
 
     Route::get('orders/{id}/send', 'App\Http\Controllers\OrdersController@send')->name('orders.send');
@@ -146,6 +144,8 @@ Route::group(['middleware' => 'user'], function () {
     Route::put('orders/{id}/address', 'App\Http\Controllers\OrdersController@updateAddress')->name('orders.update.address');
 
     Route::get('orders/{id}/generate', 'App\Http\Controllers\OrdersController@orderPdf')->name('orders.generate');
+
+    Route::post('orders/{id}/upload', 'App\Http\Controllers\OrdersController@upload')->name('orders.upload');
 
     Route::resource('orders', 'App\Http\Controllers\OrdersController');
     Route::resource('curtains', 'App\Http\Controllers\CurtainsController');;
