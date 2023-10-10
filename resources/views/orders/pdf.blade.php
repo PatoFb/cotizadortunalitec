fca58c3c384f8bd7957c9f6271316e9c5f4cd3d9
Pato Laptop <spfautschb@gmail.com>
2023-10-10 15:17:31 -0600

    more pdf styling

--- resources/views/orders/pdf.blade.php
+++ resources/views/orders/pdf.blade.php
@@ -277,6 +277,260 @@
         </tr>
         </tbody>
     </table>
+    <table class="pdf-table">
+        <thead>
+        <tr>
+            <th class="border-right">Datos de sistema:</th>
+            @if(sizeof($order->curtains) >= 12)
+                @for($i = 7; $i <= 12; $i++)
+                    <th class="border-bottom">Sistema {{$i}}:</th>
+                @endfor
+            @else
+                @for($i = 7; $i <= sizeof($order->curtains); $i++)
+                    <th class="border-bottom">Sistema {{$i}}:</th>
+                @endfor
+            @endif
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td class="border-right">Cantidad</td>
+            @if(sizeof($order->curtains) >= 12)
+                @for($i = 6; $i < 12; $i++)
+                    <td class="text-right">{{$order->curtains[$i]->quantity}}</td>
+                @endfor
+            @else
+                @for($i = 6; $i < sizeof($order->curtains); $i++)
+                    <td class="text-right">{{$curtain->quantity}}</td>
+                @endfor
+            @endif
+        </tr>
+        <tr>
+            <td class="border-right">Modelo</td>
+            @if(sizeof($order->curtains) >= 12)
+                @for($i = 6; $i < 12; $i++)
+                    <td class="text-right">{{$order->curtains[$i]->model->name}}</td>
+                @endfor
+            @else
+                @for($i = 6; $i < sizeof($order->curtains); $i++)
+                    <td class="text-right">{{$curtain->model->name}}</td>
+                @endfor
+            @endif
+        </tr>
+        <tr>
+            <td class="border-right">Modelo</td>
+            @if(sizeof($order->curtains) >= 12)
+                @for($i = 6; $i < 12; $i++)
+                    <td class="text-right">{{$order->curtains[$i]->mechanism->name}}</td>
+                @endfor
+            @else
+                @for($i = 6; $i < sizeof($order->curtains); $i++)
+                    <td class="text-right">{{$curtain->mechanism->name}}</td>
+                @endfor
+            @endif
+        </tr>
+        <tr>
+            <td class="border-right">Mecanismo</td>
+            @if(sizeof($order->curtains) >= 12)
+                @for($i = 6; $i < 12; $i++)
+                    <td class="text-right">{{$order->curtains[$i]->mechanism->name}}</td>
+                @endfor
+            @else
+                @for($i = 6; $i < sizeof($order->curtains); $i++)
+                    <td class="text-right">{{$curtain->mechanism->name}}</td>
+                @endfor
+            @endif
+        </tr>
+        <tr>
+            <td class="border-right">Ancho</td>
+            @if(sizeof($order->curtains) >= 12)
+                @for($i = 6; $i < 12; $i++)
+                    <td class="text-right">{{$order->curtains[$i]->width}} m</td>
+                @endfor
+            @else
+                @for($i = 6; $i < sizeof($order->curtains); $i++)
+                    <td class="text-right">{{$curtain->mechanism->width}} m</td>
+                @endfor
+            @endif
+        </tr>
+        <tr>
+            <td class="border-right">Caída</td>
+            @if(sizeof($order->curtains) >= 12)
+                @for($i = 6; $i < 12; $i++)
+                    <td class="text-right">{{$order->curtains[$i]->height}} m</td>
+                @endfor
+            @else
+                @for($i = 6; $i < sizeof($order->curtains); $i++)
+                    <td class="text-right">{{$curtain->mechanism->height}} m</td>
+                @endfor
+            @endif
+        </tr>
+        <tr>
+            <td class="border-right"></td>
+            @if(sizeof($order->curtains) >= 12)
+                @for($i = 6; $i < 12; $i++)
+                    <td class="text-right">{{$order->curtains[$i]->cover->id}} m</td>
+                @endfor
+            @else
+                @for($i = 6; $i < sizeof($order->curtains); $i++)
+                    <td class="text-right">{{$curtain->mechanism->cover->id}} m</td>
+                @endfor
+            @endif
+        </tr>
+        <tr>
+            <td class="border-right">Cubierta</td>
+            @if(sizeof($order->curtains) >= 12)
+                @for($i = 6; $i < 12; $i++)
+                    <td class="text-right">{{$order->curtains[$i]->cover->name}} m</td>
+                @endfor
+            @else
+                @for($i = 6; $i < sizeof($order->curtains); $i++)
+                    <td class="text-right">{{$curtain->mechanism->cover->name}} m</td>
+                @endfor
+            @endif
+        </tr>
+        <tr>
+            <td class="border-right fixed-height-cell"></td>
+            @if(sizeof($order->curtains) >= 12)
+                @for($i = 6; $i < 12; $i++)
+                    <td></td>
+                @endfor
+            @else
+                @for($i = 6; $i < sizeof($order->curtains); $i++)
+                    <td></td>
+                @endfor
+            @endif
+        </tr>
+        <tr>
+            <td class="border-right">Manivela</td>
+            @if(sizeof($order->curtains) >= 12)
+                @for($i = 6; $i < 12; $i++)
+                    @if($order->curtains[$i]->handle_id != 9999 && $order->curtains[$i]->handle_id != 999)
+                        <td class="text-right">{{$order->curtains[$i]->handle->measure}} m ({{$order->curtains[$i]->handle_quantity}})</td>
+                    @else
+                        <td></td>
+                    @endif
+                @endfor
+            @else
+                @for($i = 6; $i < sizeof($order->curtains); $i++)
+                    @if($curtain->handle_id != 9999 && $curtain->handle_id != 999)
+                        <td class="text-right">{{$curtain->handle->measure}} m ({{$curtain->handle_quantity}})</td>
+                    @else
+                        <td></td>
+                    @endif
+                @endfor
+            @endif
+        </tr>
+        <tr>
+            <td class="border-right">Control</td>
+            @if(sizeof($order->curtains) >= 12)
+                @for($i = 6; $i < 12; $i++)
+                    @if($order->curtains[$i]->control_id != 9999 && $order->curtains[$i]->control_id != 999)
+                        <td class="text-right">{{$order->curtains[$i]->control->name}} m ({{$order->curtains[$i]->control_quantity}})</td>
+                    @else
+                        <td></td>
+                    @endif
+                @endfor
+            @else
+                @for($i = 6; $i < sizeof($order->curtains); $i++)
+                    @if($curtain->control_id != 9999 && $curtain->control_id != 999)
+                        <td class="text-right">{{$curtain->control->name}} m ({{$curtain->control_quantity}})</td>
+                    @else
+                        <td></td>
+                    @endif
+                @endfor
+            @endif
+        </tr>
+        <tr>
+            <td class="border-right">Control voz</td>
+            @if(sizeof($order->curtains) >= 12)
+                @for($i = 6; $i < 12; $i++)
+                    @if($order->curtains[$i]->voice_id != 9999 && $order->curtains[$i]->voice_id != 999)
+                        <td class="text-right">{{$order->curtains[$i]->voice->name}} m ({{$order->curtains[$i]->voice_quantity}})</td>
+                    @else
+                        <td></td>
+                    @endif
+                @endfor
+            @else
+                @for($i = 6; $i < sizeof($order->curtains); $i++)
+                    @if($curtain->voice_id != 9999 && $curtain->voice_id != 999)
+                        <td class="text-right">{{$curtain->voice->name}} ({{$curtain->voice_quantity}})</td>
+                    @else
+                        <td></td>
+                    @endif
+                @endfor
+            @endif
+        </tr>
+        <tr>
+            <td class="border-right">Tejadillo</td>
+            @if(sizeof($order->curtains) >= 12)
+                @for($i = 6; $i < 12; $i++)
+                    @if($order->curtains[$i]->canopy == 1)
+                        <td class="text-right">Si</td>
+                    @else
+                        <td></td>
+                    @endif
+                @endfor
+            @else
+                @for($i = 6; $i < sizeof($order->curtains); $i++)
+                    @if($curtain->canopy == 1)
+                        <td class="text-right">Si</td>
+                    @else
+                        <td></td>
+                    @endif
+                @endfor
+            @endif
+        </tr>
+        <tr>
+            <td class="border-right fixed-height-cell"></td>
+            @if(sizeof($order->curtains) >= 12)
+                @for($i = 6; $i < 12; $i++)
+                    <td></td>
+                @endfor
+            @else
+                @for($i = 6; $i < sizeof($order->curtains); $i++)
+                    <td></td>
+                @endfor
+            @endif
+        </tr>
+        <tr>
+            <td class="border-right">Tipo de instalación</td>
+            @if(sizeof($order->curtains) >= 12)
+                @for($i = 6; $i < 12; $i++)
+                    <td class="text-right">{{$order->curtains[$i]->installation_type}}</td>
+                @endfor
+            @else
+                @for($i = 6; $i < sizeof($order->curtains); $i++)
+                    <td class="text-right">{{$curtain->installation_type}}</td>
+                @endfor
+            @endif
+        </tr>
+        <tr>
+            <td class="border-right">Lado del mecanismo</td>
+            @if(sizeof($order->curtains) >= 12)
+                @for($i = 6; $i < 12; $i++)
+                    <td class="text-right">{{$order->curtains[$i]->mechanism_side}}</td>
+                @endfor
+            @else
+                @for($i = 6; $i < sizeof($order->curtains); $i++)
+                    <td class="text-right">{{$curtain->mechanism_side}}</td>
+                @endfor
+            @endif
+        </tr>
+        <tr>
+            <td class="border-right">Precio</td>
+            @if(sizeof($order->curtains) >= 12)
+                @for($i = 6; $i < 12; $i++)
+                    <td bgcolor="#d3d3d3" class="text-center">${{number_format($order->curtains[$i]->price,2)}}</td>
+                @endfor
+            @else
+                @for($i = 6; $i < sizeof($order->curtains); $i++)
+                    <td bgcolor="#d3d3d3" class="text-center">${{number_format($curtain->price,2)}}</td>
+                @endfor
+            @endif
+        </tr>
+        </tbody>
+    </table>
 </div>
 </body>
 </html>
