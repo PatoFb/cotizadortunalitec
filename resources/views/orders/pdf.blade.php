ccb69a94a50d87f72b4cc589427ea6616871dc33
Pato Laptop <spfautschb@gmail.com>
2023-10-10 17:24:55 -0600

    page 1 pdf

--- resources/views/orders/pdf.blade.php
+++ resources/views/orders/pdf.blade.php
@@ -81,7 +81,7 @@
         </tbody>
     </table>
     <br><br><br>
-    <div class="pdf-section notes">
+    <div class="notes">
         <p class="red">Notas Adicionales</p>
         a) Los precios mostrados en esta Oferta incluyen IVA<br>
         b) Vigencia valida por 10 dias habilies o hasta agotar existencias<br>
@@ -91,10 +91,9 @@
         f) Confirmar disponibilidad de materiales con personal de ventas Tunali Tec antes de enviar pedido en firma para producción<br>
 
     </div>
-    <div class="pdf-section company-logo">
+    <div class="company-logo">
         <img src="{{asset('storage')}}/images/{{$order->curtains[0]->model->photo}}" style="max-width: 250px" alt="Model">
     </div>
-    <hr>
     <div class="pdf-section company-logo">
         <!-- Insert company logo here -->
         <img src="{{ asset('material') }}/img/logosolair.png" alt="Company Logo" width="200">
