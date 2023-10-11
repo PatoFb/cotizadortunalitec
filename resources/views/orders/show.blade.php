82f24a4b5cb303bc3349d8d6202d9c8fd247cfd9
Pato Laptop <spfautschb@gmail.com>
2023-10-11 13:51:06 -0600

    delete bug

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -37,9 +37,10 @@
                                   </a>
                               @if($role == 1)
                                   <button type="button" class="btn btn-sm btn-info" data-toggle="modal" data-target="#authorizeModal" id="authorize_order_modal">
-                                      Autorizar y enviar a producción
+                                      Enviar a producción
                                   </button>
                               @endif
+                                  <a href="{{route('orders.type', $order->id)}}" class="btn btn-sm btn-primary" >Agregar producto</a>
                           <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#fileModal" id="file_order_modal">
                               Agregar comprobante
                           </button>
