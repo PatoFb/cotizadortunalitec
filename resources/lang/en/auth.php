1c7b75f4cde1ff5582d278dd77d1b23de0fe4a16
Pato Laptop <spfautschb@gmail.com>
2023-10-18 10:20:34 -0600

    login error message

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
+    'failed' => 'Las credenciales ingresadas no son válidas.',
+    'password' => 'La contraseña es incorrecta.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
