5fe1051b79f38a1adee58a521d4d9fa0c9b1504b
PatoFb <spfautschb@gmail.com>
2024-10-06 22:48:23 -0600

    edit systems pages

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -90,7 +90,7 @@
               </div>
             </div>
               <div class="card-footer">
-                  <div class="d-flex justify-content-center">
+                  <div class="justify-content-center">
                       {{ $users->links() }}
                   </div>
               </div>
