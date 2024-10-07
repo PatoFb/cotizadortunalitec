9c61037df08ff96624eb56ca12af71399e6fd337
PatoFb <spfautschb@gmail.com>
2024-10-06 22:47:32 -0600

    users changes

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -89,6 +89,11 @@
                 </table>
               </div>
             </div>
+              <div class="card-footer">
+                  <div class="d-flex justify-content-center">
+                      {{ $users->links() }}
+                  </div>
+              </div>
           </div>
       </div>
     </div>
