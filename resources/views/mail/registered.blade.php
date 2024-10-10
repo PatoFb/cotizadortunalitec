e9cfd1e6ca9bb07569b31875ebb2a9a3227e1b6e
PatoFb <spfautschb@gmail.com>
2024-10-10 16:40:07 -0600

    edit user mail

--- resources/views/mail/registered.blade.php
+++ resources/views/mail/registered.blade.php
@@ -8,5 +8,13 @@
 <body>
 <p>El socio {{$user['name']}} con número {{$user['number']}} se registró exitosamente.</p>
 <p>Favor de revisar y validar la información para aprobar o eliminar el usuario lo antes posible.</p>
+<p>Para realizar esto, sigue los siguientes pasos:</p>
+<li>
+    <ul>ingresa al cotizador con tu cuenta.</ul>
+    <ul>Expande el submenú "Admin" en el menú izquierdo e ingresa a la opción de "Usuarios".</ul>
+    <ul>Busca el número de socio en la lista o ingresa su número de socio en el formato de arriba y presiona buscar.</ul>
+    <ul>En la columna de acciones, da click al lápiz verde para editar el nuevo usuario.</ul>
+    <ul>Busca la opción "Rol" (4ta opción), estará asignado como "No Autorizado". Selecciona la opción "Usuario" y da click en "Guardar"</ul>
+</li>
 </body>
 </html>
